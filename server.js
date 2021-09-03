3cae3af658e057138bbdb71846e372bf6d78065c
Mehmet Emin Yıldırım <mehmetcts@gmail.com>
2021-09-03 19:43:53 +0300

    update dummy-data

--- server.js
+++ server.js
@@ -1,14 +1,78 @@
 import { GraphQLServer } from "graphql-yoga";
 
+const Albums = require("./dummy-database/albums.json");
+const Songs = require("./dummy-database/songs.json");
+const Artists = require("./dummy-database/artists.json");
+const SongsArtist = require("./dummy-database/songs-artists.json");
+
 const typeDefs = `
   type Query {
-    hello(name: String): String!
+    song(id: ID): Song!
+    songs: [Song!]!
+    album(id: ID): Album!
+    albums: [Album!]!
+    artist(id: ID): Artist!
+    artists: [Artist!]!
+  }
+
+  type Album {
+    id: ID!
+    title: String!
+    cover: String!
+    songs: [Song!]!
+  }
+
+  type Song {
+    id: ID!
+    title: String!
+    src: String!
+    albumID: ID!
+    album: Album!
+    artists: [Artist!]!
+  }
+
+  type Artist {
+    id: ID!
+    name: String!
+    cover: String
+    songs: [Song!]
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: (_, { name }) => `Hello ${name || "World"}`,
+    song: (_, { id }) => Songs.find((song) => song.id === id),
+    songs: () => Songs,
+    album: (_, { id }) => Albums.find((album) => album.id === id),
+    albums: () => Albums,
+    artist: (_, { id }) => Artists.find((artist) => artist.id === id),
+    artists: () => Artists,
+  },
+  Album: {
+    songs: (parent) => Songs.filter((song) => song.albumID === parent.id),
+  },
+  Song: {
+    album: (parent) => Albums.find((album) => album.id === parent.albumID),
+    artists: (parent) => {
+      const artistIDs = [];
+      SongsArtist.forEach((songartist) => {
+        if (songartist.songID === parent.id) {
+          artistIDs.push(songartist.artistID);
+        }
+      });
+      return Artists.filter((artist) => artistIDs.includes(artist.id));
+    },
+  },
+  Artist: {
+    songs: (parent) => {
+      const songIDs = [];
+      SongsArtist.forEach((songartist) => {
+        if (songartist.artistID === parent.id) {
+          songIDs.push(songartist.songID);
+        }
+      });
+      return Songs.filter((song) => songIDs.includes(song.id));
+    },
   },
 };
 
